2d5e3435a8a9eadaf9bc1d96f0d4949f954cfff8
Bipin Sivaraman <bipin.sivaraman@csuglobal.edu>
2025-04-15 23:46:48 -0700

    CT CSC500-1: Program to add and subtract two numbers.

---
+++ CSC500/addition_subtraction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+CSC500-1: CT1 - Program to find the addition and subtraction of two numbers.
+
+"""
+num1 = int(input('Enter the first number :'))
+num2 = int(input('Enter the second number :'))
+result_add = num1 + num2
+result_sub = num1 - num2 
+print(f'{num1} + {num2} = {result_add}')
+print(f'{num1} - {num2} = {result_sub}')
+
