d67c1968208e28035b3424fcf9154dcce54fb389
Bipin Sivaraman <bipin.sivaraman@csuglobal.edu>
2025-04-16 00:29:22 -0700

    CT CSC500-1: Program to multiply and dividet two numbers.

---
+++ CSC500/multiplication_divistion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+CSC500-1: CT1 - Program to find the multiplication and division of two numbers.
+
+"""
+num1 = int(input('Enter the first number :'))
+num2 = int(input('Enter the second number :'))
+result_multiply = num1 * num2
+result_division = num1 / num2 
+print(f'{num1} * {num2} = {result_multiply}')
+print(f'{num1} / {num2} = {result_division}')
+
